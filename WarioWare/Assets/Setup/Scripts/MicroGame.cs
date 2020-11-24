56e09604adb9eba23fbcff1addb23032b80fcf9d
Nicolas Capelier <capelier.nicolas@gmail.com>
2020-11-24 19:44:28 +0100

    Added Unity project

---
+++ WarioWare/Assets/Setup/Scripts/MicroGame.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Micro Game", menuName = "Micro Game", order = 50)]
+public class MicroGame : ScriptableObject
+{
+    public string gameName = "";
+    public string authors = "";
+}
