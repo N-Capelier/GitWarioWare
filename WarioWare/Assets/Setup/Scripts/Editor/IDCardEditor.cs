b97e54b60c375db5e84b93c50a9bac566e0e0631
Dubois <s.dubois@rubika-edu.com>
2020-11-25 18:05:19 +0100

    ID card

---
+++ WarioWare/Assets/Setup/Scripts/Editor/IDCardEditor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using Trisibo;
+
+[CustomEditor(typeof(IDCard))]
+public class IDCardEditor : Editor {
+
+	private IDCard idCard;
+	private TrioAurelien trioAurel;
+	private TrioTheodore trioTheo;
+	private TrioThibault trioThibault;
+
+	private void OnEnable() {
+		idCard = target as IDCard;
+	}
+	
+
+	
+	public override void OnInspectorGUI()
+	{
+		if (idCard.idName == null || idCard.idName == string.Empty)
+			idCard.idName = idCard.name;
+		idCard.idName = EditorGUILayout.TextField("Nom :" ,idCard.idName);
+		idCard.cluster = (Cluster)EditorGUILayout.EnumPopup("Cluster ",idCard.cluster);
+        switch (idCard.cluster)
+        {
+            case Cluster.Theodore:
+				trioTheo = (TrioTheodore)EditorGUILayout.EnumPopup("Trio ", trioTheo);
+				idCard.trio = trioTheo.ToString();
+				break;
+            case Cluster.Aurelien:
+				trioAurel = (TrioAurelien)EditorGUILayout.EnumPopup("Trio ", trioAurel);
+				idCard.trio = trioTheo.ToString();
+				break;
+            case Cluster.Thibault:
+				trioThibault = (TrioThibault)EditorGUILayout.EnumPopup("Trio ", trioThibault);
+				idCard.trio = trioTheo.ToString();
+				break;
+            default:
+                break;
+        }
+		EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(idCard.microGameScene)));
+		EditorUtility.SetDirty(idCard);
+		Repaint();
+		serializedObject.ApplyModifiedProperties();
+    }
+}
