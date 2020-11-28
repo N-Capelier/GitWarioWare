54ebdf06733c8c62e4bd40965ee2400e2de4d932
sam <s.dubois@rubika-edu.com>
2020-11-28 15:00:54 +0100

    parent script

---
+++ WarioWare/Assets/MacroGame/TimedBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Testing;
+using UnityEngine.SceneManagement;
+
+public class TimedBehavior : MonoBehaviour
+{
+    float bpm = 60;
+    Manager.difficulty currentDifficulty = 0;
+  
+    private float timer;
+
+    public virtual void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "TestingScene")
+        {
+            bpm = Manager.Instance.bpm;
+            currentDifficulty = Manager.Instance.currentDifficulty;
+        }
+    }
+
+
+    public virtual void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 60 / bpm)
+        {
+            timer = 0;
+            TimedUpdate();
+        }
+    }
+
+    /// <summary>
+    /// TimedUdpate is call at each tic. Use this if you want your script to update with rithme
+    /// </summary>
+    public virtual void TimedUpdate()
+    {
+
+    }
+
+}
