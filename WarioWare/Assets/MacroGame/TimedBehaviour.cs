845b92515c73da41e4d2e5a2fe96230f45d208de
sam <s.dubois@rubika-edu.com>
2020-11-29 16:53:19 +0100

    security for updates

--- WarioWare/Assets/MacroGame/TimedBehaviour.cs
+++ WarioWare/Assets/MacroGame/TimedBehaviour.cs
@@ -9,21 +9,45 @@ public class TimedBehaviour : MonoBehaviour
    [HideInInspector] public float bpm = 60;
     [HideInInspector] public Manager.difficulty currentDifficulty = 0;
 
-   
+    private bool isInPlayableScene;
    public double timer;
     public double currentTime;
     public virtual void Start()
     {
         if (SceneManager.GetActiveScene().name == "TestingScene")
         {
+            isInPlayableScene = true;
             bpm =(float) Manager.Instance.bpm;
             currentDifficulty = Manager.Instance.currentDifficulty;
+            currentTime = AudioSettings.dspTime;
         }
-        currentTime = AudioSettings.dspTime;
     }
 
 
     public virtual void FixedUpdate()
+    {
+        if (isInPlayableScene)
+            UpdatePlayableScene();
+        else
+            UpdateCustomScene();
+    }
+    #region SecuredUpdates
+    /// <summary>
+    /// Update on a custom scene of trio
+    /// </summary>
+    private void UpdateCustomScene()
+    {
+        timer = Time.deltaTime;
+        if (timer >= 60 / bpm)
+        {
+            timer = 0;
+            TimedUpdate();
+        }
+    }
+    /// <summary>
+    /// Update on a scen with manager
+    /// </summary>
+    private void UpdatePlayableScene()
     {
         timer = AudioSettings.dspTime - currentTime;
         if (timer >= 60 / bpm)
@@ -32,6 +56,7 @@ public class TimedBehaviour : MonoBehaviour
             TimedUpdate();
         }
     }
+    #endregion
 
     /// <summary>
     /// TimedUdpate is call at each tic. Use this if you want your script to update with rithme
