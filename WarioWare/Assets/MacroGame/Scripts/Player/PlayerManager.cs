ffb7c127f3dd2b6e7eb108025b5bcb404a7fb6fc
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-12-06 16:52:19 +0100

    added PlayerManager (hp, coins, food + basic methods) + basic UI

---
+++ WarioWare/Assets/MacroGame/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Player
+{
+    public class PlayerManager : Singleton<PlayerManager>
+    {
+        #region Variables
+
+        public int playerHp;
+        public int beatcoins;
+        public int food;
+
+        #endregion
+
+        private void Awake()
+        {
+            CreateSingleton(true);
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        #region CustomMethods
+        public void TakeDamage(int damage)
+        {
+            playerHp -= damage;
+        }
+        public void GainCoins(int coins)
+        {
+            beatcoins += coins;
+        }
+        public void GainFood(int f)
+        {
+            food += f;
+        }
+        #endregion
+    }
+}
+
