94a12480f036e00dc72db5877fe9b7c40ee31474
Nicolas Capelier <capelier.nicolas@gmail.com>
2020-12-08 11:40:05 +0100

    Finished Island Creation

--- WarioWare/Assets/MacroGame/Scripts/Islands/IslandCreator.cs
+++ WarioWare/Assets/MacroGame/Scripts/Islands/IslandCreator.cs
@@ -34,11 +34,16 @@ namespace Islands
         {
             //Convert weights to percentages
             int _totalWeight = commonRewardRateWeight + epicRewardRateWeight + legendaryRewardRateWeight;
-            if(_totalWeight != 100)
+            int _commonRate, _epicRate;
+            if (_totalWeight != 100)
             {
-                int _commonRate = commonRewardRateWeight * 100 / _totalWeight;
-                int _epicRate = epicRewardRateWeight * 100 / _totalWeight;
-                int _legendaryRate = legendaryRewardRateWeight * 100 / _totalWeight;
+                _commonRate = commonRewardRateWeight * 100 / _totalWeight;
+                _epicRate = epicRewardRateWeight * 100 / _totalWeight;
+            }
+            else
+            {
+                _commonRate = commonRewardRateWeight;
+                _epicRate = epicRewardRateWeight;
             }
 
             //Split rewards from rarity
@@ -46,9 +51,9 @@ namespace Islands
             List<Reward> _epicRewards = new List<Reward>();
             List<Reward> _legendaryRewards = new List<Reward>();
 
-            for(int i = 0; i < gameRewards.Length; i++)
+            for (int i = 0; i < gameRewards.Length; i++)
             {
-                switch(gameRewards[i].rarity)
+                switch (gameRewards[i].rarity)
                 {
                     case RewardRarity.Common:
                         _commonRewards.Add(gameRewards[i]);
@@ -69,8 +74,59 @@ namespace Islands
 
             for (int i = 0; i < islands.Length; i++)
             {
-
+                float index = i / islands.Length * 100;
+                if (index <= _commonRate)
+                {
+                    _generatedReward[i] = _commonRewards[Random.Range(0, _commonRewards.Count)];
+                }
+                else if (index <= _epicRate)
+                {
+                    _generatedReward[i] = _epicRewards[Random.Range(0, _epicRewards.Count)];
+                }
+                else
+                {
+                    _generatedReward[i] = _legendaryRewards[Random.Range(0, _legendaryRewards.Count)];
+                }
             }
+
+            //Shuffle the rewards
+            _generatedReward = FisherYates(_generatedReward);
+
+            //Spread the rewards to the islands
+            for (int i = 0; i < islands.Length; i++)
+            {
+                Sprite _islandSprite;
+                switch (_generatedReward[i].rarity)
+                {
+                    case RewardRarity.Common:
+                        _islandSprite = islandSprites[0];
+                        break;
+                    case RewardRarity.Epic:
+                        _islandSprite = islandSprites[1];
+                        break;
+                    case RewardRarity.Legendary:
+                        _islandSprite = islandSprites[2];
+                        break;
+                    default:
+                        throw new System.Exception("Sprite out of bounds !");
+                }
+
+                islands[i].SetReward(_generatedReward[i], _islandSprite);
+            }
+        }
+
+        Reward[] FisherYates(Reward[] _rewards)
+        {
+            for (int i = _rewards.Length - 1; i > 0; i--)
+            {
+                int _rnd = Random.Range(0, i);
+
+                Reward _temp = _rewards[i];
+
+                _rewards[i] = _rewards[_rnd];
+                _rewards[_rnd] = _temp;
+            }
+            return _rewards;
         }
     }
 }
