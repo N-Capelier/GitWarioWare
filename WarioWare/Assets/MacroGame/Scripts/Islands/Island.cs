4dbe05f2dc302df1062e6119d64550c471ca21bb
Nicolas Capelier <capelier.nicolas@gmail.com>
2020-12-08 10:55:35 +0100

    Added reward System & Island logic

--- WarioWare/Assets/MacroGame/Scripts/Islands/Island.cs
+++ WarioWare/Assets/MacroGame/Scripts/Islands/Island.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewards;
 
 namespace Islands
 {
-    public enum Difficulty
+    public enum IslandDifficulty
     {
         Easy = 0,
         Medium = 1,
@@ -12,6 +13,56 @@ namespace Islands
     }
     public class Island : MonoBehaviour
     {
-        
+        #region Variables
+
+        //Variables
+        IslandDifficulty difficulty;
+        Reward reward;
+
+        [SerializeField] Island[] neighbours;
+
+        //Components
+        SpriteRenderer spriteRenderer;
+
+        #endregion
+
+        #region Unity Messages
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Init the island with this method.
+        /// </summary>
+        /// <param name="_reward"></param>
+        public void SetReward(Reward _reward, Sprite _sprite)
+        {
+            reward = _reward;
+            switch (_reward.rarity)
+            {
+                case RewardRarity.Common:
+                    difficulty = IslandDifficulty.Easy;
+                    spriteRenderer.sprite = _sprite;
+                    break;
+                case RewardRarity.Epic:
+                    difficulty = IslandDifficulty.Medium;
+                    spriteRenderer.sprite = _sprite;
+                    break;
+                case RewardRarity.Legendary:
+                    difficulty = IslandDifficulty.Hard;
+                    spriteRenderer.sprite = _sprite;
+                    break;
+                default:
+                    throw new System.Exception("Rarity not linked to difficulty !");
+            }
+        }
+
+        #endregion
     }
 }
---
+++ WarioWare/Assets/MacroGame/Scripts/Islands/IslandCreator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Rewards;
+
+namespace Islands
+{
+    [DisallowMultipleComponent]
+    public class IslandCreator : Singleton<IslandCreator>
+    {
+        [Header("Game Elements")]
+
+        [SerializeField] Island[] islands;
+        [SerializeField] Sprite[] islandSprites;
+
+        [SerializeField] Reward[] gameRewards;
+
+        [Header("Procedural Generation")]
+
+        [SerializeField] [Range(0, 100)] int commonRewardRateWeight;
+        [SerializeField] [Range(0, 100)] int epicRewardRateWeight;
+        [SerializeField] [Range(0, 100)] int legendaryRewardRateWeight;
+
+        private void Awake()
+        {
+            CreateSingleton();
+        }
+
+        private void Start()
+        {
+            GenerateRewards();
+        }
+
+        void GenerateRewards()
+        {
+            //Convert weights to percentages
+            int _totalWeight = commonRewardRateWeight + epicRewardRateWeight + legendaryRewardRateWeight;
+            if(_totalWeight != 100)
+            {
+                int _commonRate = commonRewardRateWeight * 100 / _totalWeight;
+                int _epicRate = epicRewardRateWeight * 100 / _totalWeight;
+                int _legendaryRate = legendaryRewardRateWeight * 100 / _totalWeight;
+            }
+
+            //Split rewards from rarity
+            List<Reward> _commonRewards = new List<Reward>();
+            List<Reward> _epicRewards = new List<Reward>();
+            List<Reward> _legendaryRewards = new List<Reward>();
+
+            for(int i = 0; i < gameRewards.Length; i++)
+            {
+                switch(gameRewards[i].rarity)
+                {
+                    case RewardRarity.Common:
+                        _commonRewards.Add(gameRewards[i]);
+                        break;
+                    case RewardRarity.Epic:
+                        _epicRewards.Add(gameRewards[i]);
+                        break;
+                    case RewardRarity.Legendary:
+                        _legendaryRewards.Add(gameRewards[i]);
+                        break;
+                    default:
+                        throw new System.Exception("Missing reward rarity !");
+                }
+            }
+
+            //Generate the rewards depending on their drop rate
+            Reward[] _generatedReward = new Reward[islands.Length];
+
+            for (int i = 0; i < islands.Length; i++)
+            {
+
+            }
+        }
+    }
+}
