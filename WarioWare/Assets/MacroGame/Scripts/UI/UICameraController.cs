45f3d52e6b3e830c52df1369bb810d6d4fc9451f
Depraz <t.deprazdepland@rubika-edu.com>
2020-12-10 10:39:15 +0100

    Fix namespace

--- WarioWare/Assets/MacroGame/Scripts/UI/UICameraController.cs
+++ WarioWare/Assets/MacroGame/Scripts/UI/UICameraController.cs
@@ -3,61 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-public class UICameraController : MonoBehaviour
+namespace UI
 {
-
-    private float horizontalMove;
-    private float verticalMove;
-
-    public CinemachineVirtualCamera UICamera;
-    public RectTransform mapCanvas;
-    public Transform targetTransform;
-    public Transform playerTransform;
-    public float cameraSpeed;
-    public float joystickDeadZone;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class UICameraController : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontalMove = Input.GetAxis("Left_Joystick_X");
-        verticalMove = Input.GetAxis("Left_Joystick_Y");
+        private float horizontalMove;
+        private float verticalMove;
 
-        if(Mathf.Abs(horizontalMove) < joystickDeadZone && Mathf.Abs(verticalMove) < joystickDeadZone)
+        public CinemachineVirtualCamera UICamera;
+        public RectTransform mapCanvas;
+        public Transform targetTransform;
+        public Transform playerTransform;
+        public float cameraSpeed;
+        public float joystickDeadZone;
+
+
+        // Start is called before the first frame update
+        void Start()
         {
-            targetTransform.position = playerTransform.position;
-        }
-        else
-        {
-            MoveTarget();
+
         }
 
-        if(!IsInsideMap())
+        // Update is called once per frame
+        void Update()
         {
-            targetTransform.position = playerTransform.position;
-        }
-    }
+            horizontalMove = Input.GetAxis("Left_Joystick_X");
+            verticalMove = Input.GetAxis("Left_Joystick_Y");
 
-    void MoveTarget()
-    {
-        targetTransform.position += new Vector3(horizontalMove, verticalMove, 0) * cameraSpeed * Time.deltaTime;
-    }
+            if (Mathf.Abs(horizontalMove) < joystickDeadZone && Mathf.Abs(verticalMove) < joystickDeadZone)
+            {
+                targetTransform.position = playerTransform.position;
+            }
+            else
+            {
+                MoveTarget();
+            }
 
-    private bool IsInsideMap()
-    {
-        if(Mathf.Abs(targetTransform.position.x) > mapCanvas.rect.width / 2 || Mathf.Abs(targetTransform.position.y) > mapCanvas.rect.height / 2)
-        {
-            return false;
+            if (!IsInsideMap())
+            {
+                targetTransform.position = playerTransform.position;
+            }
         }
-        else
+
+        void MoveTarget()
         {
-            return true;
-        }        
+            targetTransform.position += new Vector3(horizontalMove, verticalMove, 0) * cameraSpeed * Time.deltaTime;
+        }
+
+        private bool IsInsideMap()
+        {
+            if (Mathf.Abs(targetTransform.position.x) > mapCanvas.rect.width / 2 || Mathf.Abs(targetTransform.position.y) > mapCanvas.rect.height / 2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
+
