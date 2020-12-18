e514ac1f00a64d255c3e1d25ee6ff30e973b8cd0
Nicolas Capelier <capelier.nicolas@gmail.com>
2020-12-18 10:44:50 +0100

    Added resources rewards

---
+++ WarioWare/Assets/MacroGame/Scripts/Rewards/Resource/BeatcoinReward.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+namespace Rewards
+{
+    [CreateAssetMenu(fileName = "New Beatcoin", menuName = "Reward/Resource/Beatcoin", order = 50)]
+    public class BeatcoinReward : Reward
+    {
+        [SerializeField] int beatcoinAmount;
+        public override void ApplyActiveEffect()
+        {
+
+        }
+
+        public override void ApplyPassiveEffect()
+        {
+            PlayerManager.Instance.GainCoins(beatcoinAmount);
+        }
+
+        public override void GetDescription()
+        {
+
+        }
+
+        public override void RemovePassiveEffect()
+        {
+
+        }
+    }
+}
---
+++ WarioWare/Assets/MacroGame/Scripts/Rewards/Resource/HealReward.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+namespace Rewards
+{
+    [CreateAssetMenu(fileName = "New Heal", menuName = "Reward/Resource/Heal", order = 50)]
+    public class HealReward : Reward
+    {
+        [SerializeField] int healAmount;
+        public override void ApplyActiveEffect()
+        {
+            
+        }
+
+        public override void ApplyPassiveEffect()
+        {
+            PlayerManager.Instance.Heal(healAmount);
+        }
+
+        public override void GetDescription()
+        {
+            
+        }
+
+        public override void RemovePassiveEffect()
+        {
+            
+        }
+    }
+}
