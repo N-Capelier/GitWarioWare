5e7f831370e6eea4871031d58bda4d8bc8ff31e8
Deleye Arthur <arthurdeleye@gmail.com>
2020-12-27 22:28:29 +0100

    UI & island visual update

---
+++ WarioWare/Assets/MacroGame/Scripts/UI/Visual_IslandDescriptionOpening.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using DG.Tweening;
+
+public class Visual_IslandDescriptionOpening : MonoBehaviour
+{
+    [SerializeField] private RectTransform parchemin;
+
+    //for parchemin.sizeDelta
+    [SerializeField] private float closedSize = 210f;
+    [SerializeField] private float openedSize = 890f;
+
+
+    private void OnEnable()
+    {
+        parchemin.DOScaleY(0.3f, 1);
+    }
+
+    private void OnDisable()
+    {
+        parchemin.DOScaleY(0.05f, 1);
+    }
+}
