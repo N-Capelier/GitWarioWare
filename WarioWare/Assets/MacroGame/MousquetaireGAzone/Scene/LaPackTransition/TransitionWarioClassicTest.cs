46e1e5b1853ce08d4fa32edfe1eaadff2c7fd481
Deleye Arthur <arthurdeleye@gmail.com>
2020-12-27 12:28:12 +0100

    TryForWarioWareTransition

---
+++ WarioWare/Assets/MacroGame/MousquetaireGAzone/Scene/LaPackTransition/TransitionWarioClassicTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+using DG.Tweening;
+
+public class TransitionWarioClassicTest : MonoBehaviour
+{
+    [SerializeField] private RawImage renderTexture;
+    [SerializeField] private int TargetSceneIndex;
+
+
+    [SerializeField] Vector2 CenterPos = Vector2.zero;
+    [SerializeField] float fullScreenSize = 1f;
+    [SerializeField] Ease easeType = Ease.Linear;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadSceneAsync(TargetSceneIndex,LoadSceneMode.Additive);
+            CenterViewport(renderTexture.rectTransform, 1f);
+        }
+    }
+
+    public void CenterViewport(RectTransform canvas,  float duration)
+    {
+        canvas.DOAnchorPosX(CenterPos.x, duration, false).SetEase(easeType);
+        canvas.DOAnchorPosY(CenterPos.y, duration, false).SetEase(easeType);
+        canvas.DOScale(fullScreenSize, duration).SetEase(easeType);
+    }
+}
