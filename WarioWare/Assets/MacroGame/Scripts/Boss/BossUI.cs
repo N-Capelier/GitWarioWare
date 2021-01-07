36b606cabc37d0d1bea87d6d40bc126da62d522b
Dubois <s.dubois@rubika-edu.com>
2021-01-07 17:10:42 +0100

    Boss Life

---
+++ WarioWare/Assets/MacroGame/Scripts/Boss/BossUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Boss
+{
+    public class BossUI : MonoBehaviour
+    {
+        public RectTransform bossHpBackground;
+        public RectTransform bossCurrentHpParent;
+
+        public void UpdateHp(float maxHp, float currentHp)
+        {           
+            bossCurrentHpParent.DOScaleX(bossCurrentHpParent.transform.localScale.x * currentHp / maxHp,0.5f);
+        }
+
+
+    }
+}
