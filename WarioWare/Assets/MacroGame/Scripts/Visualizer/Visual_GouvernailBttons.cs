7fdb38f6274c24ab20283ba9366decf2b8476ad2
Arthur D <arthurdeleye@gmail.com>
2021-01-07 18:52:54 +0100

    MainMenue edit

---
+++ WarioWare/Assets/MacroGame/Scripts/Visualizer/Visual_GouvernailBttons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visual_GouvernailBttons : MonoBehaviour
+{
+    public RectTransform gouvernail = null;
+    [SerializeField] private RectTransform myButton = null;
+    public float gouvAngle;
+
+    private void Awake()
+    {
+        myButton = this.gameObject.GetComponent<RectTransform>();
+        gouvAngle = gouvernail.rotation.eulerAngles.z;
+
+    }
+
+    void Update()
+    {
+        float nexGouvAngle = gouvernail.rotation.eulerAngles.z;
+
+        float deltaRotation = -1* (nexGouvAngle - gouvAngle);
+
+        //myButton.rotation.eulerAngles.Set(0,0,-gouvAngle);
+        //myButton.rotation.SetEulerRotation(new Vector3 (0,0, -gouvAngle));
+        //myButton.localEulerAngles.SetZ(-gouvAngle);
+        //myButton.localRotation.SetEulerAngles(0, 0, -gouvAngle);
+        myButton.Rotate(new Vector3(0, 0, deltaRotation));
+
+        gouvAngle = nexGouvAngle;
+    }
+}
