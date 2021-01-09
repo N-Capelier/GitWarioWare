da90f59b40a77fd1c73625accd08899070306a54
Nicolas Capelier <capelier.nicolas@gmail.com>
2021-01-10 00:41:01 +0100

    Imported ClusterThibaut part 2

---
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/Cannonballs/Cannonballs_Scripts/DifficultyManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrioLLL
+{
+    namespace Cannonballs
+    {
+
+    
+
+        public class DifficultyManager : TimedBehaviour
+        {
+            public GameObject Player;
+            public GameObject GabaritRandomD1;
+            public GameObject GabaritRandomD2;
+            public GameObject GabaritFollow;
+            public GameObject GabaritPosition;
+            public override void Start()
+            {
+                base.Start(); //Do not erase this line!
+                switch (currentDifficulty)
+                {
+                    case Difficulty.EASY:
+                        Instantiate(GabaritRandomD1, Player.transform.position, Quaternion.identity);
+                        break;
+                    case Difficulty.MEDIUM:
+                        Instantiate(GabaritRandomD2, Player.transform.position, Quaternion.identity);
+                        Instantiate(GabaritFollow, GabaritPosition.transform.position, Quaternion.identity);
+                        break;
+                    case Difficulty.HARD:
+                        Instantiate(GabaritRandomD2, Player.transform.position, Quaternion.identity);
+                        Instantiate(GabaritFollow, Player.transform.position, Quaternion.identity);
+                        break;
+                }
+            }
+
+            //FixedUpdate is called on a fixed time.
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate(); //Do not erase this line!
+
+            }
+
+            //TimedUpdate is called once every tick.
+            public override void TimedUpdate()
+            {
+
+            }
+        }
+    }
+}
---
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioLLL/Cannonballs/Cannonballs_Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrioLLL
+{
+    namespace CannonBalls
+    {
+        public class PlayerController : MonoBehaviour
+        {
+            public AudioClip footPrints;
+            [SerializeField]
+            private float speed;
+            [SerializeField]
+            private float speedModifier;
+
+            private Rigidbody2D rb;
+
+            // Start is called before the first frame update
+            void Start()
+            {
+                rb = GetComponent<Rigidbody2D>();
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                Move();
+            }
+
+            private void Move()
+            {
+
+                float inputHorizontal = Input.GetAxis("Right_Joystick_X");
+                float inputVertical = Input.GetAxis("Right_Joystick_Y");
+                rb.velocity = new Vector2(inputHorizontal, inputVertical).normalized * (speed * speedModifier);
+            }
+        }
+    }
+}
