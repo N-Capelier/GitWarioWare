82e8dd99f1ec62a23aa3f4655691c2917f924144
Nicolas Capelier <capelier.nicolas@gmail.com>
2021-01-09 23:25:19 +0100

    Imported ClusterTheodore part 1/2

---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioRadioRTL/EatingContest/ScriptsEatingContest/LoseCondition.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Caps;
+
+namespace TrioRadioRTL
+{
+    namespace EatingContest
+    {
+        public class LoseCondition : TimedBehaviour
+        {
+            bool win;
+            public PlayerController player;
+            public override void Start()
+            {
+                base.Start(); //Do not erase this line!
+                
+            }
+
+            //FixedUpdate is called on a fixed time.
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate(); //Do not erase this line!
+                win = player.win;
+            }
+
+            //TimedUpdate is called once every tick.
+            public override void TimedUpdate()
+            {
+                if (Tick == 8)
+                {
+                    Debug.Log("win = " + win);
+                    if (win)
+                    {
+                        Manager.Instance.Result(true);
+                    }
+                    else
+                    {
+                        Manager.Instance.Result(false);
+                    }
+                    
+                }
+            }
+        }
+    }
+}
