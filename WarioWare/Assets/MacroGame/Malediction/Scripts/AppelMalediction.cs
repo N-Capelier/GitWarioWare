ae31b6cff75dfe9fc5807cbc15d60a6a800213d3
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2021-01-15 13:29:12 +0100

    Added Embruns + Appel Curse + fix Menu scene

---
+++ WarioWare/Assets/MacroGame/Malediction/Scripts/AppelMalediction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sound;
+
+public class AppelMalediction : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SoundManager.Instance.ApplyAudioClip("AppelCurse", audioSource);
+        //audioSource.PlaySecured();
+    }
+
+}
