ca1ee9c2cb2e8f761124d2e61378e8fb4b687117
Nicolas Capelier <capelier.nicolas@gmail.com>
2021-01-25 16:48:42 +0100

    Added KeystoneReward

---
+++ WarioWare/Assets/MacroGame/Scripts/Rewards/Resource/KeystoneReward.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+namespace Rewards
+{
+    [CreateAssetMenu(fileName = "New Keystone", menuName = "Reward/Resource/Keystone", order = 50)]
+    public class KeystoneReward : Reward
+    {
+        public override bool ApplyActiveEffect()
+        {
+            return false;
+        }
+
+        public override void ApplyPassiveEffect()
+        {
+            PlayerManager.Instance.GainKeyStone();
+        }
+
+        public override string GetDescription()
+        {
+            return "Affaibli le galion champion.";
+        }
+
+        public override void RemovePassiveEffect()
+        {
+
+        }
+    }
+}
