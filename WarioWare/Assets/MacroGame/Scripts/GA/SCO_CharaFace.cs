1748e4805b32ba67d144b01302629efc4d5105f5
Arthur D <arthurdeleye@gmail.com>
2021-01-27 11:24:28 +0100

    Character faces integrated

---
+++ WarioWare/Assets/MacroGame/Scripts/GA/SCO_CharaFace.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "NewCharaFaces", menuName = "GameArtist/CharaFaces")]
+public class SCO_CharaFace : ScriptableObject
+{
+    //List à edit (ne pas oublie d'edit aussi le script de base)
+    [Space(10)]
+    public Sprite faceHappy, faceSad, faceNeutral, faceAngry;
+
+}
